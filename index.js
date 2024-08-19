bc203b8edfd16c927fa1e03301a15b01602eddf7
Seth Strouf <seth.strouf@doane.edu>
2024-08-19 16:33:15 -0600

    Complete 3.8

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
-app.use(morgan('tiny'))
+
+morgan.token('data', (req, res) => { return JSON.stringify(req.body)})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 
 let persons = [
   {
