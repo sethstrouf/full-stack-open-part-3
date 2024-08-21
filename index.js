d193129677acbfb455ec05bb3d6e2007947de589
Seth Strouf <seth.strouf@doane.edu>
2024-08-21 16:26:58 -0600

    Complete 3.14

--- index.js
+++ index.js
@@ -27,53 +27,32 @@ app.get('/api/persons/:id', (req, res) => {
   })
 })
 
-const generateId = () => {
-  const max = 1000000
-  const randomId = Math.floor(Math.random() * max)
-  return String(randomId)
-}
-
-const findExistingPerson = (newPersonName) => {
-  return persons.find(person => person.name === newPersonName)
-}
-
 app.post('/api/persons', (req, res) => {
   const body = req.body
 
-  if (!body.name) {
-    return res.status(400).json({
-      error: 'name missing'
-    })
+  if (body.name === undefined) {
+    return res.status(400).json({ error: 'name missing' })
   }
 
-  if (!body.number) {
-    return res.status(400).json({
-      error: 'number missing'
-    })
+  if (body.number === undefined) {
+    return res.status(400).json({ error: 'number missing' })
   }
 
-  if (findExistingPerson(body.name)) {
-    return res.status(400).json({
-      error: 'person already exists'
-    })
-  }
-
-  const person = {
+  const person = new Person({
     name: body.name,
-    number: body.number,
-    id: generateId(),
-  }
+    number: body.number
+  })
 
-  persons = persons.concat(person)
-
-  res.json(person)
+  person.save().then(savedPerson => {
+    res.json(savedPerson)
+  })
 })
 
 app.delete('/api/persons/:id', (req, res) => {
-  const id = req.params.id
-  persons = persons.filter(person => person.id !== id)
-
-  res.status(204).end()
+  Person.findByIdAndDelete(req.params.id).then(returnedPerson => {
+    res.json(returnedPerson)
+    res.status(204).end()
+  })
 })
 
 const PORT = process.env.PORT
