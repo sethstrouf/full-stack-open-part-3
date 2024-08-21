2c77d92ba169c1a4447c030ca6726fa13a30adac
Seth Strouf <seth.strouf@doane.edu>
2024-08-21 17:04:49 -0600

    Complete 3.15 and 3.16

--- index.js
+++ index.js
@@ -15,19 +15,23 @@ morgan.token('data', (req, res) => { return JSON.stringify(req.body)})
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 
-app.get('/api/persons', (req, res) => {
-  Person.find({}).then(persons => {
-    res.json(persons)
-  })
+app.get('/api/persons', (req, res, next) => {
+  Person.find({})
+    .then(persons => {
+      res.json(persons)
+    })
+    .catch(error => next(error))
 })
 
-app.get('/api/persons/:id', (req, res) => {
-  Person.findById(req.params.id).then(person => {
-    res.json(person)
-  })
+app.get('/api/persons/:id', (req, res, next) => {
+  Person.findById(req.params.id)
+    .then(person => {
+      res.json(person)
+    })
+    .catch(error => next(error))
 })
 
-app.post('/api/persons', (req, res) => {
+app.post('/api/persons', (req, res, next) => {
   const body = req.body
 
   if (body.name === undefined) {
@@ -43,17 +47,38 @@ app.post('/api/persons', (req, res) => {
     number: body.number
   })
 
-  person.save().then(savedPerson => {
-    res.json(savedPerson)
-  })
+  person.save()
+    .then(savedPerson => {
+      res.json(savedPerson)
+    })
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (req, res) => {
-  Person.findByIdAndDelete(req.params.id).then(returnedPerson => {
-    res.json(returnedPerson)
-    res.status(204).end()
-  })
+app.delete('/api/persons/:id', (req, res, next) => {
+  Person.findByIdAndDelete(req.params.id)
+    .then(returnedPerson => {
+      res.json(returnedPerson)
+      res.status(204).end()
+    })
+    .catch(error => next(error))
 })
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  if (error.name === 'UnknownEndpoint') {
+    return response.status(404).send({ error: 'unknown endpoint' })
+  }
+
+  next(error)
+}
+
+// This must be the last loaded middleware and all routes must be registered before this
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
