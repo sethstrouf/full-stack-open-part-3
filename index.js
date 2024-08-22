8c8c4d667bfe9f8608afcf92f8e0bb24122d0b36
Seth Strouf <seth.strouf@doane.edu>
2024-08-21 19:40:56 -0600

    Complete 3.17

--- index.js
+++ index.js
@@ -54,6 +54,21 @@ app.post('/api/persons', (req, res, next) => {
     .catch(error => next(error))
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
 app.delete('/api/persons/:id', (req, res, next) => {
   Person.findByIdAndDelete(req.params.id)
     .then(returnedPerson => {
